2f390c4518978ef49cffca0a708fedf930036aea
Midship62 <colinjeanpatrick@gmail.com>
2019-04-01 13:26:22 +0200

    up

--- admin/reservations.php
+++ admin/reservations.php
@@ -1,14 +1,12 @@
 <?php
-try
-{
-  $bdd = new PDO('mysql:host=localhost;dbname=bar-sans-titre;charset=utf8', 'root', 'philippe');
-}
-catch (Exception $e)
-{
-  die('Erreur : ' . $e->getMessage());
-}
-  ?>
-  <?php
-  $retour = $bdd->prepare("SELECT COUNT(*) AS total FROM reservation");
-  $retour->execute();
+include_once('includes/header.php');
+?>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+
+</div>
+
+<?php
+include_once('includes/footer.php');
 ?>
