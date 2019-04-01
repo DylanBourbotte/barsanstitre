c213588e994e85163e01507c9eb4bd733090a7fa
parshimipopeli <philippe.lamorskipro@gmail.com>
2019-04-01 12:35:06 +0200

    add sql

--- includes/config.php
+++ includes/config.php
@@ -1,4 +1,15 @@
 <?php 
+try
+{
+  $bdd = new PDO('mysql:host=localhost;dbname=bar-sans-titre;charset=utf8', 'root', 'philippe');
+}
+catch (Exception $e)
+{
+  die('Erreur : ' . $e->getMessage());
+  ?>
+  <?php
+  $retour = $bdd->prepare("SELECT COUNT(*) AS total FROM reservation");
+$retour->execute();
 
 $footernav = [
     "mention.php" => "Mentions légales",
