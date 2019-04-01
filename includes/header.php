3f0485b98bde4be09d4262e2de1d16f130305c70
Ledoux <louyledouxsimplon@gmail.com>
2019-04-01 16:59:56 +0800

    nouveau

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/png" href="img/logonoir.png">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
     <title>Le bar sans titre - ROOFTOP</title>
 </head>
 <body>
@@ -13,6 +14,24 @@
 
 
 <header>
+<div class="toggle-btn">
+  <span class="one"></span>
+  <span class="two"></span>
+</div>
+
+<div class="menuContent">
+  <div class="data">
+    <nav>
+    <ul>
+      <li><a class='leA' href="#">A propos</a></li>
+      <li><a class='leA' href="#">Rooftop</a></li>
+      <li><a class='leA' href="#">About</a></li>
+      <li><a class='leA' href="#">Team</a></li>
+    </ul>
+    </nav>
+  </div>
+</div>
+
 </header>
 
 <div class="banner">
