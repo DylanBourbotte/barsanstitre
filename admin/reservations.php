1f3a70d5a3903f9baba73f6045f94c13f03226a4
parshimipopeli <philippe.lamorskipro@gmail.com>
2019-04-01 12:21:36 +0200

    admin:reservation

---
+++ admin/reservations.php
@@ -0,0 +1,13 @@
+<?php
+try
+{
+  $bdd = new PDO('mysql:host=localhost;dbname=bar-sans-titre;charset=utf8', 'root', 'philippe');
+}
+catch (Exception $e)
+{
+  die('Erreur : ' . $e->getMessage());
+  ?>
+  <?php
+  $retour = $bdd->prepare("SELECT COUNT(*) AS total FROM reservation");
+$retour->execute();
+?>
