f46d96f0179b36f38659a5ae7ef0f045587156c6
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-04-02 13:43:47 +0200

    ajout encore est encore

--- includes/header.php
+++ includes/header.php
@@ -53,20 +53,8 @@ require('includes/config.php');
   </div>
 </div>
 
-<<<<<<< HEAD
-
-=======
 <div id="navDeskop"></div>
->>>>>>> be97a6b64dfa50234dc52d8c316127de3d58d711
 </header>
 
 
 </div> <!-- Bg header -->
-
-$(function(){
-  const nav = $("#nav").html();
-
-  $("#navDeskop").html(nav); 
-
-
-});
