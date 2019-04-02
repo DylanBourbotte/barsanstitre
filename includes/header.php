d5521a0ac1f19e71cd6308c36ad7702a2c90930f
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-04-02 14:39:42 +0200

    lol

--- includes/header.php
+++ includes/header.php
@@ -15,49 +15,4 @@ require('includes/config.php');
 
 
 
-<div class="etiquettelogo">
-  <img src="./img/logonoir.png" alt="logo du bar">
-</div>
-<div class="bgHeader">
-<div class="navDeskop">
-<nav class='nav'>
-    <ul class='flex'>
-      <li><a class='leADeskop' href="#about">A propos</a></li>
-      <li><a class='leADeskop' href="#rooftop">Rooftop</a></li>
-      <li><a class='leADeskop' href="#product">Produits</a></li>
-      <li><a class='leADeskop' href="contact.php">Contact</a></li>
-      <li><a class='leADeskop' href="#">Réservation</a></li>
-    </ul>
-    </nav>
-</div>
-<div class="logobar">
-  <!-- <img src="../img/logonoir.png" alt=""> -->
-</div>
-<header>
-<div class="toggle-btn">
-  <span class="one"></span>
-  <span class="two"></span>
-</div>
 
-<div class="menuContent">
-  <div class="data">
-    <nav id="nav">
-    <ul>
-      <li><a class='leA' href="#about">A propos</a></li>
-      <li><a class='leA' href="#rooftop">Rooftop</a></li>
-      <li><a class='leA' href="#product">Produits</a></li>
-      <li><a class='leA' href="contact.php">Contact</a></li>
-      <li><a class='leA' href="#">Réservation</a></li>
-    </ul>
-    </nav>
-  </div>
-</div>
-
-<<<<<<< HEAD
-=======
-<div id="navDeskop"></div>
->>>>>>> f46d96f0179b36f38659a5ae7ef0f045587156c6
-</header>
-
-
-</div> <!-- Bg header -->
