9869208bc79b67f7ba0f5aeebf5ae2a079b8098c
unknown <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-04-05 09:46:25 +0200

    ajout des span sur l'accueil pour les mot clé important

--- index.php
+++ index.php
@@ -7,31 +7,34 @@ include('includes/header.php');
     <div class="row">
     <div class="col-12 text-center titlebar">Le bar sans titre</div>
         <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12 accueil">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, eum voluptates autem doloremque velit deserunt magnam voluptate, minus maxime modi odit. Quod nesciunt excepturi minus quasi deserunt? Hic, ut dolorum.
-            Molestiae accusantium quia, esse, id deserunt similique provident maiores consequuntur rem repudiandae minus magnam impedit consequatur? Modi ab ducimus voluptatem ipsum totam blanditiis, a saepe facere est inventore iure velit?
+            <span class="test">Lorem</span> ipsum dolor sit amet consectetur, adipisicing elit. Voluptas, eum voluptates autem doloremque velit deserunt magnam voluptate, minus maxime modi odit. Quod nesciunt excepturi minus quasi deserunt? Hic, ut dolorum.
+            Molestiae accusantium quia, esse, id <span class="test">deserunt similique</span> provident maiores consequuntur rem repudiandae minus magnam impedit consequatur? Modi ab ducimus voluptatem ipsum totam blanditiis, a saepe facere est inventore iure velit?
             Autem sit atque facere voluptas ratione. Vel cupiditate soluta enim nihil. Molestias, iure, doloremque nemo veniam dicta ipsum rem cum error consectetur sapiente vitae, recusandae accusamus beatae natus maxime. Doloremque.
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia sunt culpa ducimus minima sequi qui quod ea. Architecto, velit itaque. Dolores, sint beatae quod repellendus rerum quas sed in assumenda.Deleniti delectus veritatis autem maiores quisquam quaerat magni animi laudantium laboriosam, repellendus quos provident impedit unde. Porro nihil impedit veritatis ea suscipit voluptate explicabo natus laborum inventore, sint aut quis!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia sunt culpa ducimus minima <span class="test">sequi qui quod ea.</span> Architecto, velit itaque. Dolores, sint beatae quod repellendus rerum quas sed in assumenda.Deleniti delectus veritatis autem maiores quisquam quaerat magni animi laudantium laboriosam, repellendus quos provident impedit unde. Porro nihil impedit veritatis ea suscipit voluptate explicabo natus laborum inventore, sint aut quis!
         </div>
-        <div id="carouselExampleControls" class="carousel slide col-lg-6 col-md-12 col-sm-12 col-xs-12" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="img/greg.jpg" class="d-block w-100" alt="...">
+        
+    <div id="carouselExampleControls" class="carousel slide col-lg-6 col-md-12 col-sm-12 col-xs-12" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="img/greg.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/greg2.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
     </div>
-    <div class="carousel-item">
-      <img src="img/greg2.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-    </div>
 </div>
+
+
 </section>
 </main>
 
