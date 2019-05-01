0790e395bcf8b5710a0488fa1480fb5b172855c8
Dylan Bourbotte <dylanbourbotte.simplon@gmail.comgit config --global user.name Dylan>
2019-05-01 15:39:29 +0200

    ajout d'une page d'acces restreint

---
+++ admin/403.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Non autorisée</title>
+    <link rel="stylesheet" href="403.css">
+</head>
+<body>
+<div class="lock"></div>
+<div class="message">
+  <h1>L'acces a cette page est restreint</h1>
+  <p>Vérifier d'avoir les droit sur cette page. <a href="../index.php">Retournez au site</a></p>
+</div>
+</body>
+</html>
--- admin/secret.php
+++ admin/secret.php
@@ -2,7 +2,7 @@
     if(isset($_POST['mot_de_passe']) && $_POST['mot_de_passe'] == "57Wkk8L" && isset($_POST['nom']) && $_POST['nom'] == "Greg") {
         header('Location: admin.php');
     } else {
-        echo "<b>Désolé, le code secret et incorrect</b>";
+        header('Location: 403.php');
     }
 
 ?>
